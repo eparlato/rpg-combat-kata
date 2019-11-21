71e3baeb8534fa6ed92b1bba1a33ed1c73137adc
Edoardo Parlato <edoardo.parlato@xpeppers.com>
2019-11-21 14:12:27 +0100

    When damage received exceeds current health health becomes zero

--- src/test/java/CharacterTest.java
+++ src/test/java/CharacterTest.java
@@ -1,25 +1,38 @@
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.core.Is.is;
 
 public class CharacterTest {
+
+    public static final int DAMAGE_EXCEEDING_HEALTH = Character.INITIAL_HEALTH + 1;
+    private Character character;
+
+    @Before
+    public void setUp() {
+        character = new Character();
+    }
+
     @Test
     public void has_health_at_1000_when_created() {
-        Character character = new Character();
-
         assertThat(character.health(), is(Character.INITIAL_HEALTH));
     }
 
     @Test
     public void loses_health_when_receiving_damage() {
-        Character character = new Character();
-
         int currentHealth = character.health();
 
         character.receiveDamage(1);
 
         assertThat(character.health(), is(currentHealth-1));
+    }
+
+    @Test
+    public void when_damage_received_exceeds_current_health_health_becomes_zero() {
+        character.receiveDamage(DAMAGE_EXCEEDING_HEALTH);
+
+        assertThat(character.health(), is(0));
     }
 }
 
@@ -37,5 +50,9 @@ class Character {
 
     public void receiveDamage(int damage) {
         health -= damage;
+
+        if (health < 0) {
+            health = 0;
+        }
     }
 }
